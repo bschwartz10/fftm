2c0861c12b2dd435fb08c949734b3635a07d3775
bschwartz10 <bschwartz92@gmail.com>
2017-07-29 16:32:48 -0600

    add onClick function to traded player component

--- src/TradedPlayer.jsx
+++ src/TradedPlayer.jsx
@@ -6,12 +6,21 @@ export default class TradedPlayer extends Component {
     let position = this.props.info.position
     let nflTeam = this.props.info.nflTeam
     let fantasyPoints = this.props.info.fantasyPoints
+    let teamId = this.props.info.teamId
+
     return (
       <tr className="tradedPlayer">
        <td>{name}</td>
        <td>{position}</td>
        <td>{nflTeam}</td>
        <td>{fantasyPoints} points</td>
+       <td>
+         <button
+           className="button remove-player"
+           onClick={() => this.props.onClick({name: name, teamId: teamId})}>
+           Remove
+         </button>
+       </td>
       </tr>
     )
   }
