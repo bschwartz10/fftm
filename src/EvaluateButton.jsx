08a75d992eedf3d00c4964e09cb789d43c13ed01
bschwartz10 <bschwartz92@gmail.com>
2017-07-30 14:03:48 -0600

    WIP trade result

--- src/EvaluateButton.jsx
+++ src/EvaluateButton.jsx
@@ -1,21 +1,48 @@
 import React, { Component } from 'react';
+import {TradeResult} from './TradeResult.jsx'
 
-export const EvaluateButton = (props) => {
-  if (props.teamOne.length >= 1 && props.teamTwo.length >= 1) {
-    return (
-      <div>
-        <button className="button">Evaluate Trade</button>
-        <button className="button reset-trade" onClick={() => props.onClick()}>
-          Reset Trade
-        </button>
-      </div>
-    )
-  } else {
+export default class EvaluateButton extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {
+        showTrade: false
+    }
+    this.handleShowTrade = this.handleShowTrade.bind(this)
+  }
+
+  handleShowTrade(input) {
+    this.setState({showTrade: input})
+  }
+
+  totalPoints() {
+    return this.props.teamOne.reduce((total, player) => {
+      return total + player.fantasyPoints
+    }, 0)
+  }
+
+  tradeResult() {
+    console.log('fired')
+    this.handleShowTrade(true)
+  }
+
+  render() {
+    if (this.props.teamOne.length >= 1 && this.props.teamTwo.length >= 1) {
       return (
         <div>
-          <button className="button disabled">Evaluate Trade</button>
-          <button className="button disabled reset-trade">Reset Trade</button>
+          <button className="button" onClick={() => this.tradeResult()}>Evaluate Trade</button>
+          <button className="button reset-trade" onClick={() => this.props.onClick()}>
+            Reset Trade
+          </button>
+          { this.state.showTrade ? <TradeResult points={this.totalPoints()} /> : null }
         </div>
       )
+    } else {
+        return (
+          <div>
+            <button className="button disabled">Evaluate Trade</button>
+            <button className="button disabled reset-trade">Reset Trade</button>
+          </div>
+        )
+    }
   }
 }
